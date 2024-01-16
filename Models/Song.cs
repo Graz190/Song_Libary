91c193f8d0b0ba04eda6425fb309da8e8a55a250
Graz190 <alucard190@web.de>
2024-01-16 18:05:13 +0100

    First Prototyp

---
+++ Models/Song.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Song_Libary.Models
+{
+    internal class Song
+    {
+        public int Id { get; set; }
+        public string SongName { get; set; }
+        public string RecordName { get; set;}
+        public int RecordNumber {  get; set;}
+        public DateTime PublishDate { get; set; }
+        public string ImageLink {  get; set; }
+        public string BandName {  get; set; }
+
+        public Song(int id, string songName, string recordName, int recordNumber, DateTime publishDate, string imageLink, string bandName)
+        {
+            Id = id;
+            SongName = songName;
+            RecordName = recordName;
+            RecordNumber = recordNumber;
+            PublishDate = publishDate;
+            ImageLink = imageLink;
+            BandName = bandName;
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Song_Libary.Models;
+using Song_Libary.Utils;
+
+namespace Song_Libary.ViewModels
+{
+    internal class MainViewModel : PropertyChangedBase
+    {
+        public MainViewModel() { Songs = [
+            new Song(1, "Testname", "TestRecord", 1, new DateTime(1990, 01, 01), "link", "BandNameTest"),
+            new Song(2, "Testname", "TestRecord", 2, new DateTime(1990, 01, 01), "link", "BandNameTest"),
+            new Song(3, "Testname", "TestRecord", 3, new DateTime(1990, 01, 01), "link", "BandNameTest"),
+            new Song(4, "Testname", "TestRecord", 4, new DateTime(1990, 01, 01), "link", "BandNameTest")
+            ]; }
+        private int viewType;
+
+        public int ViewType
+        {
+            get { return viewType; }
+            set
+            {
+                viewType = value;
+                NotifyOfPropertyChange("ViewType");
+            }
+        }
+        public ObservableCollection<Song> Songs { get; set; }
+
+    }
+}
